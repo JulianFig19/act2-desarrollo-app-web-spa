f7288ab6cc14c953333b9c50f023da8e3fdefa5e
Julian Figueroa <julianfigxix@gmail.com>
2025-05-19 09:17:55 -0500

    Corrección Landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Landing from './Vistas/landing'
+import Landing from './Vistas/Landing'
 import Footer from './Componentes/Footer/Footer'
 import './App.css'
 
